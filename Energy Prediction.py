c310b2447ebe2cc4af2f2daa14bb4f80547d4f33
Shaunak Badani <shaunak.badani@reserch.iiit.ac.in>
2021-04-09 23:09:34 +0530

    Reorganized folder structure, improved script

--- Energy Prediction.py
+++ Energy Prediction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[30]:
+# In[1]:
 
 
 import numpy as np
@@ -10,83 +10,71 @@ import matplotlib.pyplot as plt
 
 # ## Importing the dataset
 
-# In[69]:
+# In[2]:
 
 import sys
+#name = 'benzene_old_dft'
+#mol = 'Benzene'
 name = sys.argv[1]
+mol = sys.argv[2]
 
 
-# In[70]:
+# In[3]:
 
 
-benzene_data = np.load("{}.npz".format(name))
+data = np.load(name)
 
 
-# In[71]:
+# In[4]:
 
 
-print(benzene_data.files)
+print(data.files)
 
 
-# In[72]:
+# In[5]:
 
 
-for i in benzene_data.files:
-    print(i, benzene_data[i].shape)
+for i in data.files:
+    print(i, data[i].shape)
 
 
-# In[73]:
+# In[6]:
 
 
-configs = benzene_data['R']
+configs = data['R']
 
 
-# In[74]:
+# In[13]:
 
 
-fig = plt.figure(figsize = (15, 10))
+fig = plt.figure(figsize = (15, 12))
+
+plt.title("{} configurations, with Energies".format(mol), fontsize = 20)
 
 for num in range(6):
     ax = fig.add_subplot(2, 3, num + 1, projection = '3d')
     i = configs[num]
     ax.scatter(i[:, 0], i[:, 1], i[:, 2])
-    ax.set_title("Energy : {}".format(benzene_data['E'][num][0]))
-plt.savefig('configurations.png')
+    ax.set_title("Energy : {}".format(data['E'][num][0]))
+plt.savefig("images/{}_configurations.png".format(mol))
+# plt.xticks('off')
 
 
 # ### Label statistics
 
-# In[75]:
+# In[15]:
 
 
-benzene_data['E'].shape
+print(data['E'].shape)
 
 
-# In[76]:
-
-
-hist, a = np.histogram(benzene_data['E'], bins = 10)
-
-
-# In[77]:
-
-
-print(a)
-
-
-# In[78]:
-
-
-print(hist)
-
-
-# In[82]:
+# In[20]:
 
 
 from matplotlib import colors
 
 
-# In[80]:
+# In[23]:
 
 
 fig, axs = plt.subplots(1, 1,
@@ -99,7 +87,7 @@ axs.grid(b = True, color ='grey',
         linestyle ='-.', linewidth = 0.5, 
         alpha = 0.6) 
 
-N, bins, patches = axs.hist(benzene_data['E'], bins = 30)
+N, bins, patches = axs.hist(data['E'], bins = 30)
 fracs = ((N**(1 / 5)) / N.max())
 norm = colors.Normalize(fracs.min(), fracs.max())
 axs.xaxis.set_ticks_position('none') 
@@ -112,7 +100,7 @@ for thisfrac, thispatch in zip(fracs, patches):
 axs.set_xlabel("Energy Values", fontsize = 20)
 axs.set_ylabel("Frequency", fontsize = 20)
 
-axs.set_title("Benzene Label Statistics", fontsize = 30)
+axs.set_title("{} Label Statistics".format(mol), fontsize = 30)
 
-plt.savefig("{}_label_statistics.png".format(name))
+plt.savefig("images/{}_label_statistics.png".format(mol))
 
