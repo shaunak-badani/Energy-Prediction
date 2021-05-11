98f5cafd9677bb608d8bc4426b3e828ed778f519
Shaunak Badani <shaunak.badani@research.iiit.ac.in>
2021-05-11 23:44:25 +0530

    Added previous models

---
+++ models/3_Lower_LR_MLP/run_model.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -A research
+#SBATCH --qos=medium
+#SBATCH -n 20
+#SBATCH --gres=gpu:2
+#SBATCH --mem-per-cpu=2048
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=END
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/shaunak/miniconda2/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/shaunak/miniconda2/etc/profile.d/conda.sh" ]; then
+        . "/home/shaunak/miniconda2/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/shaunak/miniconda2/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+conda activate ML
+
+jupyter nbconvert --to script Training.ipynb
+mv Training.txt Training.py
+python3 Training.py
+
+
+
